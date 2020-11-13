70131d2c382b4527dad93e38ac921f55fc472630
Sammy Abdulkader <sammy.abdulkader@philips.com>
2020-11-13 14:39:02 -0500

    TDD - test development code for shopping items

---
+++ src/main/java/Item.java
@@ -0,0 +1,14 @@
+public class Item {
+
+    private final double unitPrice;
+    private final int quantity;
+
+    public Item(double unitPrice, int quantity) {
+        this.unitPrice = unitPrice;
+        this.quantity = quantity;
+    }
+
+    double getSubtotal() {
+        return unitPrice * (double) quantity;
+    }
+}
